e72f5523edf0768cc6246611a924076df3e46b14
abdallah-zaghloul <3bdallahzaghloul@gmail.com>
2024-04-25 12:20:13 +0200

    redirect to google auth using server side

--- server/composables/googleService.ts
+++ server/composables/googleService.ts
@@ -2,15 +2,14 @@ import { google } from "googleapis"
 import { Setting } from "@prisma/client"
 
 const getOAuth2Client = (setting: Setting) => new google.auth.OAuth2({
-  clientId: setting?.clientId,
-  clientSecret: setting?.clientSecret,
-  // redirectUri: process.env.NUXT_YOUCAN_API_REDIRECT,
+  clientId: setting.clientId,
+  clientSecret: setting.clientSecret,
   redirectUri: process.env.GOOGLE_APP_REDIRECT_URL
 })
 
 const getAuthUrl = (setting: Setting) => getOAuth2Client(setting).generateAuthUrl({
   access_type: 'offline',
-  state: setting?.storeId,
+  state: setting.storeId,
   scope: [
     'https://www.googleapis.com/auth/userinfo.email', //user email info permission
     'https://www.googleapis.com/auth/spreadsheets' //spreedsheet permission
--- server/routes/setting.post.ts
+++ server/routes/setting.post.ts
@@ -1,7 +1,10 @@
 import { Setting } from "@prisma/client";
 import settingService from "../composables/settingService";
+import { navigateTo } from "nuxt/app";
+import googleService from "../composables/googleService";
 
 export default defineEventHandler(async (event) => {
-    const setting: Setting = await readBody(event)
-    return await settingService.set(setting.storeId, setting)
+    const reqBody: Setting = await readBody(event)
+    const setting = await settingService.set(reqBody.storeId, reqBody)
+    return !setting.isConnected ? await sendRedirect(event, googleService.getAuthUrl(setting)) : setting
 });
