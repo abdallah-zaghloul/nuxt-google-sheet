b68b91c141876fc6310b68f8b12ae3f1f5216e17
Eihab Khan <143792300+eihabkhan1@users.noreply.github.com>
2024-05-03 15:16:13 +0100

    add sheets initial layout

---
+++ components/ConnectedUser.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { SecondaryButton } from '@youcan/ui-vue3'
+
+</script>
+
+<template>
+  <div class="container">
+    <p>Connected as: <span>wiw@wiwi.com</span></p>
+    <SecondaryButton class="disconnect">
+      <template #icon>
+        <i class="icon i-youcan:sign-out"></i>
+      </template>
+      Disconnect
+    </SecondaryButton>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.container p {
+  font: var(--text-sm-regular);
+}
+
+.container p span {
+  color: var(--brand-500);
+}
+
+.container .disconnect {
+  border-color: var(--brand-500);
+}
+
+:deep(
+  .disconnect.base-button.secondary .text, 
+  .disconnect.base-button.secondary .icon
+) {
+  color: var(--brand-500);
+}
+
+:deep(
+  .disconnect.base-button.secondary:hover
+) {
+  background-color: var(--brand-500);
+}
+</style>
--- components/Sheets.vue
+++ components/Sheets.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    Sheets table
-  </div>
+  <Card class="inner-card">
+    <template #default>
+      Sheets table
+    </template>
+  </Card>
 </template>
+
+<style scoped>
+.inner-card {
+  background-color: #fcfcfc;
+  border-color: #e5e5e5;
+  box-shadow: unset;
+}
+.inner-card .main-header {
+  font-size: 15px;
+}
+</style>
