c8d2c576774020ce552f064b04d0f5d3ac88d7e8
Eihab Khan <143792300+eihabkhan1@users.noreply.github.com>
2024-05-06 16:07:29 +0100

    Feature/sheets page (#6)
    * minor typography improvement
    
    * add sheets initial layout
    
    * update disconnect button styles
    
    * minor card header accessory improvement
    
    * add static table and empty state

--- pages/index.vue
+++ pages/index.vue
@@ -8,20 +8,28 @@ const setting = useSetting(getSetting.value)
 
 <template>
   <div id="main-container" class="container">
-    <Card>
+    <Card v-if="!setting?.isConnected">
       <template #header>
         <h2 class="main-header">
           Google sheets
         </h2>
       </template>
       <template #default>
-        <div v-if="!setting?.isConnected">
-          <SetupGuide />
-          <Setting />
-        </div>
-        <div v-else>
-          <Sheets />
-        </div>
+        <SetupGuide />
+        <Setting />
+      </template>
+    </Card>
+    <Card v-else>
+      <template #header>
+        <h2 class="main-header">
+          Google sheets
+        </h2>
+      </template>
+      <template #headerAccessory>
+        <ConnectedUser />
+      </template>
+      <template #default>
+        <Sheets />
       </template>
     </Card>
   </div>
