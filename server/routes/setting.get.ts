3828e004faa728dcd6fa282fc8524339fa1af01a
abdallah-zaghloul <3bdallahzaghloul@gmail.com>
2024-05-06 14:21:50 +0300

    get setting using async handler

--- server/routes/setting.get.ts
+++ server/routes/setting.get.ts
@@ -1,6 +1,6 @@
 import settingService from "../composables/settingService";
 
-export default defineEventHandler(async (event) => {
+export default defineEventHandler((event) => handler.async(event, async () => {
     const storeId = event.context.session?.storeId
     return await settingService.get(storeId)
-});
+}))
