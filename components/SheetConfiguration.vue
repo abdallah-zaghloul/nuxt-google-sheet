a496bf67985fa9a2645e9e94a2c2f90f4b72a9f9
Eihab Khan <143792300+eihabkhan1@users.noreply.github.com>
2024-05-07 17:21:01 +0100

    add draggable

--- components/SheetConfiguration.vue
+++ components/SheetConfiguration.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
+import draggable from "vuedraggable";
 import { InputGroup, Input, MultiselectDropdown, SecondaryButton } from '@youcan/ui-vue3'
+import { ref } from 'vue'
+
 
 const products: any[] = [];
+
+const allFields = ref([
+  { name: "Order date", id: 5 },
+  { name: "First name", id: 6 },
+  { name: "Last name", id: 7 },
+]);
 
 const sheet = ref({
   name: '',
   prodcuts: [],
-  fields: []
+  fields: [{ name: "Order ID", id: 1, required: true }]
 });
 </script>
 
@@ -45,16 +54,47 @@ const sheet = ref({
                 <div class="list-title">
                   <p>Selected fields</p>
                 </div>
-                <div class="list"></div>
+                <draggable 
+                  v-model="sheet.fields" 
+                  group="fields"  
+                  item-key="id"
+                  class="list">
+                  <template #item="{element}">
+                    <div class="draggable-field selected" :class="{ disabled: element.required }">
+                      <i class="i-youcan:dots-six-vertical"></i>
+                      <div class="field-content">
+                        <span>{{element.name}}</span>
+                        <span class="required" v-if="element.required">Required</span>
+                      </div>
+                    </div>
+                  </template>
+                </draggable>
               </div>
-              <div class="">
+              <div class="separator">
                 <i class="i-youcan:arrows-left-right"></i>
               </div>
               <div class="field-list all-fields">
                 <div class="list-title">
                   <p>Available fields</p>
                 </div>
-                <div class="list"></div>
+                <div class="empty-state">
+                  <span v-if="allFields.length === 0 && sheet.fields.length > 1">No more fields</span> 
+                  <span v-else-if="allFields.length === 0">No fields available</span>
+                </div>
+                <draggable
+                  v-model="allFields" 
+                  group="fields"  
+                  item-key="id"
+                  class="list">
+                  <template #item="{element}">
+                    <div class="draggable-field">
+                      <i class="i-youcan:dots-six-vertical"></i>
+                      <div class="field-content">
+                        {{element.name}}
+                      </div>
+                    </div>
+                  </template>
+                </draggable>
               </div>
             </div>
           </template>
@@ -74,6 +114,8 @@ const sheet = ref({
 
 <style scoped>
 .inner-card {
+  --card-height: 500px;
+
   background-color: #fcfcfc;
   border-color: #e5e5e5;
   box-shadow: unset;
@@ -86,27 +128,53 @@ const sheet = ref({
 }
 
 .inner-card .fields {
+  /* position: relative; */
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   gap: 16px;
 }
 
 .inner-card .fields .field-list {
+  position: relative;
   flex: 1;
   display: flex;
   flex-direction: column;
   background-color: var(--base-white);
-  min-height: 300px;
+  height: var(--card-height);
+  overflow: scroll;
   border: 1px solid var(--card-border-color);
   border-radius: 8px;
 }
 
+.inner-card .fields .all-fields .empty-state {
+  position: absolute;
+  inset-block: 50%;
+  inset-inline: 50%;
+  min-width: 100px;
+  min-height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transform: translateX(-50%);
+  color: var(--gray-500);
+}
+
+.inner-card .fields .separator {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: var(--card-height);
+}
+
 .inner-card .fields .field-list .list-title {
-  background-color: var(--base-white);
+  position: sticky;
+  top: 0;
   padding: 16px 24px;
-  border-block-end: 1px solid var(--card-border-color);
   border-radius: 8px 8px 0 0;
+  background-color: var(--base-white);
+  border-block-end: 1px solid var(--card-border-color);
+  z-index: 1;
 }
 
 .inner-card .fields .field-list .list-title p {
@@ -115,8 +183,53 @@ const sheet = ref({
 
 .inner-card .fields .field-list .list {
   flex: 1;
-  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding: 16px;
+  background-color: var(--gray-50);
+  border-radius: 0 0 8px 8px;
   box-shadow: inset 0 5px 10px var(--card-shadow);
+}
+
+.draggable-field {
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 8px;
+  padding: 8px 16px;
+  background-color: var(--base-white);
+  border: 1px solid var(--card-border-color);
+  border-radius: 8px;
+  cursor: grab;
+  transition: all 0.2s ease-in;
+}
+
+.draggable-field.disabled {
+  cursor: unset;
+  user-select: none;
+  pointer-events: none;
+  background-color: var(--gray-100);
+  border-color: var(--gray-200);
+  color: var(--gray-500)
+}
+
+.draggable-field:hover {
+  border-color: var(--brand-100);
+  background-color: var(--brand-50);
+}
+
+.draggable-field .field-content {
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.draggable-field .field-content .required {
+  font: var(--text-xs-regular);
 }
 
 .inner-card .footer {
