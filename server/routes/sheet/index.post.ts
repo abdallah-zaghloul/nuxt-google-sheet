509f7ff16c75a71fd45f6c6cfeefe32bda890120
abdallah-zaghloul <3bdallahzaghloul@gmail.com>
2024-05-09 19:20:38 +0300

    fix: don't create sheet unless setting is connected

--- server/routes/sheet/index.post.ts
+++ server/routes/sheet/index.post.ts
@@ -11,6 +11,6 @@ export default defineEventHandler((event) => handler.async(event, async () => {
   const reqBody = await validator.reqBody(sheetCreateSchema, event)
   const storeId = getStoreId(event)
   const setting = await settingService.get(storeId)
-  const googleSpreadSheet: GoogleSpreadSheet = await googleService.initClient(setting!).createSpreadSheet(reqBody.title, reqBody.headers)
-  return sheetService.create(storeId, googleSpreadSheet)
+  const googleSpreadSheet: GoogleSpreadSheet | null = await googleService.initClient(setting!).createSpreadSheet(reqBody.title, reqBody.headers)
+  return googleSpreadSheet ? sheetService.create(storeId, googleSpreadSheet) : handler.globalError(event)
 }))
