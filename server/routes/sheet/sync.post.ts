2fc44b73292c59ef71de45e3bdf4ad336571f307
abdallah-zaghloul <3bdallahzaghloul@gmail.com>
2024-05-22 17:07:57 +0300

    sync sheets

--- server/routes/sheet/sync.post.ts
+++ server/routes/sheet/sync.post.ts
@@ -3,5 +3,6 @@ import getCurrentSession from "~/server/utils/getCurrentSession"
 
 export default defineEventHandler((event) => handler.async(event, async () => {
   const youCanService = youcanService.init(await getCurrentSession(event))
+  await youCanService.unSubscribeCreateOrderSubs()
   return await youCanService.subscribeCreatedOrder() || handler.globalError(event)
 }))
