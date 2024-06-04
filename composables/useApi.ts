2f41c4b30611708f6623020912b6ea1708d17f08
abdallah-zaghloul <3bdallahzaghloul@gmail.com>
2024-06-04 15:36:19 +0300

    retrive api data:
    - getSheetHeaders
    
    - getSheet
    
    - getSheets
    
    - deleteSheet
    
    - updateSheet
    
    - manualSyncSheet

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -1,9 +1,16 @@
-//@ts-nocheck
-import type { UseFetchOptions } from "nuxt/app";
-import type { Setting, ResBody } from "../utils/types";
 import { toast } from "@youcan/ui-vue3/helpers";
-import { ResBody } from "../server/utils/types";
+import type { UseFetchOptions } from "nuxt/app";
+import type {
+  ResBody,
+  Headers,
+  Sheet,
+  SheetUpdate
+} from "../utils/types";
 
+// default pagination Count
+const paginationCount = Number(process.env.PAGINATION_COUNT ?? 1000)
+
+// global fetcher
 const fetcher = <T>(url: string, options?: UseFetchOptions<unknown>): Promise<Ref<T | null>> => useFetch(url, {
   method: "GET",
   headers: {
@@ -25,16 +32,61 @@ const fetcher = <T>(url: string, options?: UseFetchOptions<unknown>): Promise<Re
   ...options,
 
 }).then(
+  //@ts-ignore retrive data key
   ({ data: resBody }: { data: Ref<ResBody<T>> }) => ref(resBody.value.data)
 )
+
+
+
 
 export default {
 
   getSetting: () => fetcher<Setting>("/setting"),
 
+
+
   setSetting: (setting: Partial<Setting>) => fetcher<Setting>("/setting", {
     method: "POST",
     body: setting
+  }),
+
+
+
+  getSheetHeaders: () => fetcher<Headers>("/sheet/headers"),
+
+
+
+  getSheet: (id: string) => fetcher<Sheet>(`/sheet/${id}`),
+
+
+
+  getSheets: (id: string, take?: number, skip?: number) => fetcher<Sheet[]>("/sheet", {
+    query: {
+      take: take ?? paginationCount
+    },
+  }),
+
+
+
+  deleteSheet: (id: string) => fetcher<boolean>(`/sheet/${id}`, {
+    method: "DELETE"
+  }),
+
+
+
+  updateSheet: (id: string, sheetUpdate: SheetUpdate) => fetcher<Sheet>(`/sheet/${id}`, {
+    method: "PATCH",
+    body: sheetUpdate
+  }),
+
+
+
+  manualSyncSheet: (sheetId: string, orderRefs: `${number}`[]) => fetcher<Sheet>(`/sheet/manual-sync`, {
+    method: "POST",
+    body: {
+      sheetId,
+      orderRefs
+    }
   }),
 
 }
--- utils/types.ts
+++ utils/types.ts
@@ -1,13 +1,19 @@
 import type {
     Setting,
     Session,
-    ResBody as ServerResBody
+    ResBody as ServerResBody,
+    Headers,
+    Sheet,
 } from "../server/utils/types"
 
 type ResBody<T> = Pick<ServerResBody<T>, "statusCode" | "statusMessage"> & { data: Ref<T> | null }
+type SheetUpdate = Pick<Sheet, "title" | "headers">
 
 export type {
     Setting,
     Session,
-    ResBody
+    ResBody,
+    Headers,
+    Sheet,
+    SheetUpdate
 }
